bb95ee6add097485ba51abaf35f415c74f84d17e
Henry Rojas <henry.rojas.tb@gmail.com>
2025-09-07 16:12:43 -0500

    Dapper

--- Infrastructure/DataBase.cs
+++ Infrastructure/DataBase.cs
@@ -1,8 +1,10 @@
-﻿using EntitiesDomain.Responses;
+﻿using Dapper;
+using EntitiesDomain.Responses;
 using EntitiesDomain.Utils;
 using InfrastructureDomain;
 using Microsoft.Data.SqlClient;
 using System.Data;
+using static Dapper.SqlMapper;
 
 namespace Infrastructure
 {
@@ -132,6 +134,119 @@ namespace Infrastructure
                     connection.Close();
                 }
             }
+        }
+        public async Task<Response<int>> Execute(string spName, object parameters = null, int? timeOut = null)
+        {
+            Response<int> response = new();
+            try
+            {
+                Response<(SqlConnection, SqlTransaction)> getTransaction = await GetTransaction();
+                if (response.IsOk(getTransaction))
+                {
+                    response.Message = "Procedure completed successfully";
+                    response.Data = await connection.ExecuteAsync(spName, parameters, transaction, timeOut, CommandType.StoredProcedure);
+                }
+            }
+            catch (Exception ex)
+            {
+                response.AddError(ex, currentClass, nameof(Execute));
+            }
+            return response;
+        }
+        public async Task<Response<List<T>>> Query<T>(string spName, object parameters = null, int? timeOut = null)
+        {
+            Response<List<T>> response = new();
+            try
+            {
+                Response<(SqlConnection, SqlTransaction)> getTransaction = await GetTransaction();
+                if (response.IsOk(getTransaction))
+                {
+                    response.Message = "Procedure completed successfully";
+                    response.Data = (await connection.QueryAsync<T>(spName, parameters, transaction, timeOut, CommandType.StoredProcedure)).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                response.AddError(ex, currentClass, nameof(Query));
+            }
+            return response;
+        }
+        public async Task<Response<(List<A>, List<B>)>> QueryMultiple<A, B>(string spName, object parameters = null, int? timeOut = null)
+        {
+            Response<(List<A>, List<B>)> response = new();
+
+            try
+            {
+                Response<(SqlConnection, SqlTransaction)> getTransaction = await GetTransaction();
+                if (response.IsOk(getTransaction))
+                {
+                    response.Message = "Procedure completed successfully";
+                    GridReader grid = await connection.QueryMultipleAsync(spName, parameters, transaction, timeOut, CommandType.StoredProcedure);
+                    List<A> listA = grid.Read<A>().ToList();
+                    List<B> listB = grid.Read<B>().ToList();
+                    response.Data = (listA, listB);
+                }
+            }
+            catch (Exception ex)
+            {
+                response.AddError(ex, currentClass, nameof(QueryMultiple));
+            }
+            return response;
+        }
+        public async Task<Response<(List<A>, List<B>, List<C>, List<D>?, List<E>?)>> QueryMultiple<A, B, C, D, E>(string spName, object parameters = null, int? timeOut = null)
+        {
+            Response<(List<A>, List<B>, List<C>, List<D>?, List<E>?)> response = new();
+
+            try
+            {
+                Response<(SqlConnection, SqlTransaction)> getTransaction = await GetTransaction();
+                if (response.IsOk(getTransaction))
+                {
+                    response.Message = "Procedure completed successfully";
+                    GridReader grid = await connection.QueryMultipleAsync(spName, parameters, transaction, timeOut, CommandType.StoredProcedure);
+                    List<A> listA = grid.Read<A>().ToList();
+                    List<B> listB = grid.Read<B>().ToList();
+                    List<C> listC = grid.Read<C>().ToList();
+                    List<D>? listD = typeof(D) == typeof(object) ? null : grid.Read<D>().ToList();
+                    List<E>? listE = typeof(E) == typeof(object) ? null : grid.Read<E>().ToList();
+                    response.Data = (listA, listB, listC, listD, listE);
+                }
+            }
+            catch (Exception ex)
+            {
+                response.AddError(ex, currentClass, nameof(QueryMultiple));
+            }
+            return response;
+        }
+        public async Task<Response<(List<A>, List<B>, List<C>, List<D>, List<E>, List<F>, List<G>?, List<H>?, List<I>?, List<J>?)>> QueryMultiple<A, B, C, D, E, F, G, H, I, J>(string spName, object parameters = null, int? timeOut = null)
+        {
+            Response<(List<A>, List<B>, List<C>, List<D>, List<E>, List<F>, List<G>?, List<H>?, List<I>?, List<J>?)> response = new();
+
+            try
+            {
+                Response<(SqlConnection, SqlTransaction)> getTransaction = await GetTransaction();
+                if (response.IsOk(getTransaction))
+                {
+                    response.Message = "Procedure completed successfully";
+                    GridReader grid = await connection.QueryMultipleAsync(spName, parameters, transaction, timeOut, CommandType.StoredProcedure);
+                    List<A> listA = grid.Read<A>().ToList();
+                    List<B> listB = grid.Read<B>().ToList();
+                    List<C> listC = grid.Read<C>().ToList();
+                    List<D> listD = grid.Read<D>().ToList();
+                    List<E> listE = grid.Read<E>().ToList();
+                    List<F> listF = grid.Read<F>().ToList();
+                    List<G>? listG = typeof(G) == typeof(object) ? null : grid.Read<G>().ToList();
+                    List<H>? listH = typeof(H) == typeof(object) ? null : grid.Read<H>().ToList();
+                    List<I>? listI = typeof(I) == typeof(object) ? null : grid.Read<I>().ToList();
+                    List<J>? listJ = typeof(J) == typeof(object) ? null : grid.Read<J>().ToList();
+                    response.Data = (listA, listB, listC, listD, listE, listF, listG, listH, listI, listJ);
+                }
+            }
+            catch (Exception ex)
+            {
+                response.AddError(ex, currentClass, nameof(QueryMultiple));
+            }
+            return response;
         }
     }
 }
--- InfrastructureDomain/IDataBase.cs
+++ InfrastructureDomain/IDataBase.cs
@@ -7,5 +7,10 @@ namespace InfrastructureDomain
     {
         Task<Response<SqlConnection>> GetConnection();
         Task<Response<(SqlConnection, SqlTransaction)>> GetTransaction();
+        Task<Response<int>> Execute(string spName, object parameters = null, int? timeOut = null);
+        Task<Response<List<T>>> Query<T>(string spName, object parameters = null, int? timeOut = null);
+        Task<Response<(List<A>, List<B>)>> QueryMultiple<A, B>(string spName, object parameters = null, int? timeOut = null);
+        Task<Response<(List<A>, List<B>, List<C>, List<D>?, List<E>?)>> QueryMultiple<A, B, C, D, E>(string spName, object parameters = null, int? timeOut = null);
+        Task<Response<(List<A>, List<B>, List<C>, List<D>, List<E>, List<F>, List<G>?, List<H>?, List<I>?, List<J>?)>> QueryMultiple<A, B, C, D, E, F, G, H, I, J>(string spName, object parameters = null, int? timeOut = null);
     }
 }
